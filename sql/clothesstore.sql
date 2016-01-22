55b97ed8ef601469a35e48fcb81febfd26aaccec
Dulce Amada Lizeth Cortes Osuna <dulce.cortes@softtek.com>
2016-01-22 13:34:00 -0800

    tables modified

--- sql/clothesstore.sql
+++ sql/clothesstore.sql
@@ -1,26 +1,42 @@
 --Comment Liz 
 
-CREATE TABLE marca (
-id_marca int not null primary key
-nombre_marca varchar(30)
+CREATE TABLE trademark (
+trademark_id int not null primary key
+mark_name varchar(30)
 );
 
-CREATE TABLE temporada (
-id_temporada int not null primary key
-nombre_temporada varchar(30)
+CREATE TABLE season (
+season_id int not null primary key
+season_name varchar(30)
 );
 
-CREATE TABLE tallas (
-id_talla int not null primary key 
-nombre_talla varchar(30)
+CREATE TABLE size (
+size_id int not null primary key 
+size_name varchar(30)
 );
 
-CREATE TABLE articulos (
-id_articulo int not null primary key 
-nombre_articulo varchar(30),
+CREATE TABLE article (
+article_id int not null primary key 
+article_name varchar(30),
 FOREIGN KEY (id_marca) REFERENCES marca (id_marca),
 FOREIGN KEY (id_talla) REFERENCES tallas (id_talla),
-FOREIGN KEY (id_precio) REFERENCES precios (id_precio),
+FOREIGN KEY (id_precio) REFERENCES precios (id_precio)
+);
+
+CREATE TABLE sales_person (
+sales_person_id int not null primary key 
+name varchar(30),
+lastname varchar(30),
+age int,
+entry_date date,
+school_level varchar(30)
+);
+
+CREATE TABLE price (
+price_id int not null primary key 
+price varchar(30),
+FOREIGN KEY (discount) REFERENCES season_discount (discount),
+FOREIGN KEY (article) REFERENCES article (article_name)
 );
 
 CREATE TABLE season_discount (
