1c0e5d691e2947543668b568d5d51df6b2a374f8
Blanca Olivares <estelitablanca@gmail.com>
2016-01-22 12:15:17 -0800

    add tables

--- sql/clothesstore.sql
+++ sql/clothesstore.sql
@@ -1,1 +1,26 @@
 --Comment Liz 
+create table season_discount (
+season_discount_id int NOT NULL primary key,
+season_id int not null,
+discount int,
+note varchar(100)
+);
+
+create table department(
+departmentid int NOT NULL primary key,
+namedeparment varchar(30)
+);
+
+create table daysales (
+daysalesid int NOT NULL primary key,
+salesdate date,
+salesvendorid int,
+priceid int
+);
+
+create table historic (
+historicid int NOT NULL primary key,
+historicdate date,
+daysalesid int
+);
+
