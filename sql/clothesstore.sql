b10ba23a00d410e8d9c9ccf00bb202f363b8c138
Dulce Amada Lizeth Cortes Osuna <dulce.cortes@softtek.com>
2016-01-22 15:09:04 -0800

    tables repositioned

--- sql/clothesstore.sql
+++ sql/clothesstore.sql
@@ -1,30 +1,37 @@
 --Comment Liz 
 
+--drop table season
+--drop table size
+--drop table trademark
+--drop table season_discount
+--drop table comision
+--drop table daysales
+--drop table deparment
+--drop table historic
+--drop table itemsubtype
+--drop table paymenttype
+--drop table salary
+--drop table sales_person
+
 CREATE TABLE trademark (
-trademark_id int not null primary key
-mark_name varchar(30)
+trademark_id int not null primary key,
+trademark_name varchar(30)
 );
 
 CREATE TABLE season (
-season_id int not null primary key
+season_id int not null primary key,
 season_name varchar(30)
 );
 
 CREATE TABLE size (
-size_id int not null primary key 
+size_id int not null primary key,
 size_name varchar(30)
 );
 
-CREATE TABLE article (
-article_id int not null primary key 
-article_name varchar(30),
-FOREIGN KEY (id_marca) REFERENCES marca (id_marca),
-FOREIGN KEY (id_talla) REFERENCES tallas (id_talla),
-FOREIGN KEY (id_precio) REFERENCES precios (id_precio)
-);
+
 
 CREATE TABLE sales_person (
-sales_person_id int not null primary key 
+sales_person_id int not null primary key,
 name varchar(30),
 lastname varchar(30),
 age int,
@@ -32,16 +39,11 @@ entry_date date,
 school_level varchar(30)
 );
 
-CREATE TABLE price (
-price_id int not null primary key 
-price varchar(30),
-FOREIGN KEY (discount) REFERENCES season_discount (discount),
-FOREIGN KEY (article) REFERENCES article (article_name)
-);
+
 
 CREATE TABLE season_discount (
 season_discount_id int NOT NULL primary key,
-season_id int not null FOREIGN KEY REFERENCES temporada (season_id),
+season_id int not null FOREIGN KEY REFERENCES season (season_id),
 discount float,
 note varchar(100)
 );
@@ -85,6 +87,11 @@ note varchar(100),
 sales_person_id int FOREIGN KEY REFERENCES sales_person (sales_person_id)
 );
 
+create table itemsubtype (
+itemsubtype_id int,
+nameitemsubtype varchar (30)
+);
+
 --Categories like: suits, skirts, shirts, shorts, dresses, pants, coats, etc 
 create table itemtype (
 itemtype_id int NOT NULL primary key,
@@ -96,9 +103,32 @@ itemsubtype_id int FOREIGN KEY REFERENCES itemsubtype(itemsubtype_id)
 -- Contains the subcategories like:
 	-- Category= swimsuit
 		--Subcategory = bikini,  boardshorts, jammers, swim trunks, briefs or "speedos", thongs, g-strings, etc 
-create table itemsubtype (
-itemsubtype_id int,
-nameitemsubtype varchar (30)
+
+
+CREATE TABLE article (
+article_id int not null primary key,
+article_name varchar(30),
+trademark_id int FOREIGN KEY REFERENCES trademark (trademark_id),
+size_id int FOREIGN KEY REFERENCES size (size_id),
+price_id int FOREIGN KEY REFERENCES price (price_id)
 );
 
+CREATE TABLE supplier (
+supplier_id int not null primary key,
+name varchar(30),
+FOREIGN KEY (article_id) REFERENCES article (article_id)
+);
 
+CREATE TABLE warehouse (
+warehouse_id int not null primary key,
+quantity int,
+FOREIGN KEY (article_id) REFERENCES article (article_id),
+FOREIGN KEY (supplier_id) REFERENCES supplier (supplier_id)
+);
+
+CREATE TABLE price (
+price_id int not null primary key,
+price varchar(30),
+FOREIGN KEY (discount) REFERENCES season_discount (discount),
+FOREIGN KEY (article) REFERENCES article (article_name)
+);
