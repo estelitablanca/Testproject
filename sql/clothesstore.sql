1773e8702acb970a7704f3a8ca7e1a65440ae37f
Dulce Amada Lizeth Cortes Osuna <dulce.cortes@softtek.com>
2016-01-22 12:16:40 -0800

    3 tables created

--- sql/clothesstore.sql
+++ sql/clothesstore.sql
@@ -1,1 +1,6 @@
 --Comment Liz 
+CREATE TABLE marca (id_marca integer, nombre_marca varchar(30))
+
+CREATE TABLE temporada (id_temporada integer, nombre_temporada varchar(30))
+
+CREATE TABLE tallas (id_talla integer, nombre_talla varchar(30))
